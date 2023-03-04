7171c77e1bce92bcd1293791dde3abd087966f54
juzorey <josejosereyes75@gmail.com>
2023-03-04 16:41:57 -0500

    added input locations

--- uberfake/src/comp/DriverPickupLocation.jsx
+++ uberfake/src/comp/DriverPickupLocation.jsx
@@ -11,55 +11,87 @@ import {FaSearch, FaDot} from 'react-icons/fa'
 export default function DriverPickupLocation() {
 
 const{pickUpLocation, setPickUpLocation} = useContext(LocationContext)
+const[localPickupLocation, setLocalPickupLocation] =useState({
+  locationEnd: '',
+  locationStart: ''
+
+})
 
 
-  const[dropOff, setDropOff] = useState(false)
-  //this has to go into the state of map center
-  // const[pickup, setPickup] = useState(
-  //   {
-  //    location:''
-  //   }
-  // )
 
-  // const handleChange = (e)=>{
-  //   setPickup({...pickup, [e.target.id]:e.target.value})
-  //   console.log(pickup,'on')
 
   //     }
       const handleChange = (e)=>{
-        setPickUpLocation({...pickUpLocation, [e.target.id]:e.target.value})
-        console.log(pickUpLocation,'on')
-    
+        setLocalPickupLocation({...localPickupLocation, [e.target.id]:e.target.value})
         
-   
+
+      }
+
+
+      useEffect(()=>{
+
+        setPickUpLocation({...pickUpLocation, 'locationStart':localPickupLocation.locationStart, 'locationEnd':localPickupLocation.locationEnd})
+     
+        const getPickupLocationStart = async()=>{
+
+          // if(pickUpLocation.locationStart === 'e 4th street los angeles'){
+            // setPickUpLocation({...pickUpLocation, [localPickupLocation.locationEnd]:[localPickupLocation.locationEnd]})
+
+          console.log(pickUpLocation,'global new')  
+            
+          const search_text = pickUpLocation.locationStart
+          const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${search_text}.json?access_token=pk.eyJ1Ijoiam9zZXIxMTcxIiwiYSI6ImNsZW5hbXNudTFkYXgzeHF2bHpkcDJlOWQifQ.PC61kaqdxMq8ByRGyadaPQ`)
+          
+          console.log(res.data.features[0].center,'start') //res.data.features[0].bbox for bbox //res.data.features[0].center for the specific location
+          // setPickUpLocation({...pickUpLocation, locationStartLat:res.data.features[0].center[0], locationStartLng:res.data.features[0].center[1]})
+          // console.log(pickUpLocation,'on')
+      
+      
+      
+        }
+        const getPickupLocationEnd = async()=>{
+
+          // if(pickUpLocation.locationStart === 'e 4th street los angeles'){
+            // setPickUpLocation({...pickUpLocation, [localPickupLocation.locationEnd]:[localPickupLocation.locationEnd]})
+
+          console.log(pickUpLocation,'global new')  
+            
+          const search_text = pickUpLocation.locationEnd
+          const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${search_text}.json?access_token=pk.eyJ1Ijoiam9zZXIxMTcxIiwiYSI6ImNsZW5hbXNudTFkYXgzeHF2bHpkcDJlOWQifQ.PC61kaqdxMq8ByRGyadaPQ`)
+          
+          console.log(res.data.features[0].center,'end') //res.data.features[0].bbox for bbox //res.data.features[0].center for the specific location
+          // setPickUpLocation({...pickUpLocation, locationStartLat:res.data.features[0].center[0], locationStartLng:res.data.features[0].center[1]})
+          // console.log(pickUpLocation,'on')
+      
+      
+      
+        }
+        // else if(pickUpLocation.locationEnd === 'jefferson park los angeles'){
+        //   const search_text = pickUpLocation.locationEnd
+        //   const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${search_text}.json?access_token=pk.eyJ1Ijoiam9zZXIxMTcxIiwiYSI6ImNsZW5hbXNudTFkYXgzeHF2bHpkcDJlOWQifQ.PC61kaqdxMq8ByRGyadaPQ`)
+          
+        //   console.log(res.data)
+        // }
+      
+      
+      // }
+      getPickupLocationStart()
+      getPickupLocationEnd()
+  
+      },[localPickupLocation.locationEnd, localPickupLocation.locationStart])
+
+
+      console.log(pickUpLocation,'global')
+      console.log(localPickupLocation,'local')
+
+
+    // it changes url every leter and it changes the main retrieve method
+        
+
   
     
 
  //Los%20Angeles
-  const getPickupLocation = async()=>{
-
-    if(pickUpLocation.locationStart === 'e 4th street los angeles'){
-      const search_text = pickUpLocation.locationStart
-    const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${search_text}.json?access_token=pk.eyJ1Ijoiam9zZXIxMTcxIiwiYSI6ImNsZW5hbXNudTFkYXgzeHF2bHpkcDJlOWQifQ.PC61kaqdxMq8ByRGyadaPQ`)
-    
-    console.log(res.data.features[0].center) //res.data.features[0].bbox for bbox //res.data.features[0].center for the specific location
-    setPickUpLocation({...pickUpLocation, locationStartLat:res.data.features[0].center[0], locationStartLng:res.data.features[0].center[1]})
-    console.log(pickUpLocation,'on')
-
-  }
-  else if(pickUpLocation.locationEnd === 'jefferson park los angeles'){
-    const search_text = pickUpLocation.locationEnd
-    const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${search_text}.json?access_token=pk.eyJ1Ijoiam9zZXIxMTcxIiwiYSI6ImNsZW5hbXNudTFkYXgzeHF2bHpkcDJlOWQifQ.PC61kaqdxMq8ByRGyadaPQ`)
-    
-    console.log(res.data)
-  }
-
-
-}
-getPickupLocation()
-
-      }
-
   
 
 
@@ -85,7 +117,7 @@ getPickupLocation()
                 <div className="inner-search-box">
                   <form className="form-search">
                   {/* <RxDot className='icon-search' /> */}
-                    <input className="input-search" type="text" id="locationStart" value={pickUpLocation.locationStart} onChange={handleChange} onClick role="searchbox" placeholder="Starting Location" />
+                    <input className="input-search" type="text" id="locationStart" value={localPickupLocation.locationStart} onChange={handleChange} onClick role="searchbox" placeholder="Starting Location" />
                   </form>
                 </div>
               </div>
@@ -96,7 +128,7 @@ getPickupLocation()
                   <div className="inner-search-box">
                     <form className="form-search">
                       {/* <RxDot className='icon-search' /> */}
-                      <input className="input-search" type="text" role="searchbox" id="locationEnd" value={pickUpLocation.locationEnd} onChange={handleChange} placeholder="Destination" />
+                      <input className="input-search" type="text" role="searchbox" id="locationEnd" value={localPickupLocation.locationEnd} onChange={handleChange} placeholder="Destination" />
                     </form>
                   </div>
                 </div>
